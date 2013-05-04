8974d71098e95d98626bddbbef524a7470030625
sanghamithra <mithra.ss@hotmail.com>
2013-05-04 13:34:09 -0700

    changed icons img path

--- js/maps.js
+++ js/maps.js
@@ -26,9 +26,9 @@
 		    var currentLocation = FoodTruck.location,
 					map = FoodTruck.map,
 					markerIcons = {
-						'Schools':'../../img/school.png',
-						'Parking':'../../img/parking.png',
-						'Companies':'../../img/company.png'
+						'Schools':'/Prediction-analysis/img/school.png',
+						'Parking':'/Prediction-analysis/img/parking.png',
+						'Companies':'/Prediction-analysis/img/company.png'
 					} 	
 					request = {
 						location: currentLocation,
