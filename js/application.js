125b841675211e7356640726e6de723960c20839
sanghamithra <mithra.ss@hotmail.com>
2013-05-04 13:04:16 -0700

    added website files

---
+++ js/application.js
@@ -0,0 +1,101 @@
+(function(){
+
+
+
+  FoodTruck.application.initUserLogin = initUserLogin = function() {
+  	var userName = Cookies.get('foodtruck-user'),
+  		email = Cookies.get('foodtruck-user-email');
+  	if (userName) {
+  		$('.user-login-details .user-name').text(' You are logged in as '+userName);
+  		$('.user-login-details .user-logout').text('LogOut');
+  		$('.selection-list').show();
+  	}
+  	if (userName && email) {
+  		$('.user-login-details .user-email').append('('+email+')');
+  	}
+    
+  	//remove the above if loop if you want to remove email
+  }
+
+  FoodTruck.application.handleFilters = handleFilters = function(e) {
+    var $eItem = $(e.target).closest('li'),
+        data = $(e.target).text().trim();
+    if ($eItem.hasClass('expand-list'))
+        return; 
+  	$eItem.find('i').toggleClass('icon-ok');
+    if ($eItem.parent().hasClass('map-filters')) {
+      FoodTruck.maps.toggleMarkers($eItem.text());
+    } else {
+      console.log('we are going to send `'+data+'` through GET request to the server');
+      $.get('../server/getdata.java',{
+        query: data
+      }, function(data) {
+        console.log('server has returned data shown below');
+        console.log(data);
+      }).fail(function(){
+        console.log('ERROR: Bad/No response from server');
+      });
+      e.stopPropagation();
+    }
+    
+  }
+
+  FoodTruck.application.logMeIn = logMeIn = function() {
+  	var userName = $('#user-name').val(),
+  		email = $('#email').val();
+  	if (!userName) {
+  		$('#no-modal-error').show();
+  		return;
+  	}
+  	Cookies.set('foodtruck-user',userName);
+  	if (email) {
+  		Cookies.set('foodtruck-user-email',email);
+  	}
+  	initUserLogin();
+  	$('#sign-in-modal').modal('hide');
+
+  }
+
+  FoodTruck.application.logMeOut = logMeOut = function(){
+  	Cookies.set('foodtruck-user','',{expires:-6000});
+  	Cookies.set('foodtruck-user-email','',{expires:-6000});
+  	$('.user-login-details span').empty();
+  	$('.user-login-details a').empty();
+  	$('.selection-list').hide();
+    $('.sub-list-wrapper').show();
+  }
+ FoodTruck.application.handler= handler= function(event){
+    var $target =$(event.target);
+    if($target.is("li")){
+      $target.children().toggle();
+    }
+  }
+  
+  FoodTruck.application.showSubList = showSubList = function(e){
+    var $target = $(e.target).closest('li');
+    $target.children('.sub-list').toggle();
+
+  }
+  //$("display-list").click(handler).find("display-list").hide();
+
+  //FoodTruck.application.display-handlers=display-handlers=function(h){
+   // var $HItem = $(h.target).closest('li');
+   // $HItem.find('i').toggleClass('icon-ok');
+    
+
+  //}
+
+  //init function called
+  initUserLogin();
+  $('ul.sub-list').hide();
+
+  //event handlers below
+  $('.expand-list').on('click',showSubList);
+  $('ul.selection-list').on('click','li',handleFilters);
+  $('button#login-user').on('click',logMeIn);
+  $('a[class="user-logout"]').on('click',logMeOut);
+  //$('ul.display-list').on('click','ul',handleFilters);
+  //$('display-list.list1').on('click','li',handleFilters);
+
+
+}());
---
+++ js/maps.js
@@ -0,0 +1,81 @@
+(function(){
+  //this is the initialization function
+	FoodTruck.maps.init = init = function (){
+		var map,
+			currentLocation = new google.maps.LatLng(37.336, -121.89); 	
+			mapOptions = {
+	          zoom: 15,
+	          center: currentLocation,
+	          mapTypeId: google.maps.MapTypeId.HYBRID
+	        };
+	    map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+	    FoodTruck.location = currentLocation;
+	    FoodTruck.map = map;
+      FoodTruck.markers = {};
+
+	}
+
+  //this function sets the markers on the map initially
+	FoodTruck.maps.setMarkers = setMarkers = function(query) {
+		if (!query){
+      console.log('No query specified');
+      return;
+    }
+    FoodTruck.markers[query]=[];
+    var currentLocation = FoodTruck.location,
+			map = FoodTruck.map,
+			markerIcons = {
+				'Schools':'../img/school.png',
+				'Parking':'../img/parking.png',
+				'Companies':'../img/company.png'
+			} 	
+			request = {
+				location: currentLocation,
+				radius: '200',
+				query: query  	
+			};
+		service = new google.maps.places.PlacesService(map);
+		service.textSearch(request, function(data) {
+			if (!(data instanceof Array)){
+				console.log('ERROR: invalid response from server');
+				return;
+			}
+      console.log(data);
+			data.forEach(function(obj){
+				 var markerImg = markerIcons[query] ||  markerIcons['Schools'];
+				 //the second half of the or above puts schools marker as default incase markersIcons[query] is not found
+ 				 var marker = new google.maps.Marker({
+		            position: obj.geometry.location,
+		            map: map,
+		            icon: {
+		            	url: markerImg,
+		            	size: new google.maps.Size(15,15)
+		            },
+		            title: obj.name
+		        });
+ 				 marker.setVisible(false);
+         FoodTruck.markers[query].push(marker);
+			});
+		});
+	}
+
+  //this switches markers on/off on the map based on the user selected filter
+  FoodTruck.maps.toggleMarkers = toggleMarkers = function(query) {
+    if (!FoodTruck.markers[query]) {
+      console.log('marker does not exist');
+      return;
+    }
+    FoodTruck.markers[query].forEach(function(marker){
+      marker.getVisible() ? marker.setVisible(false) : marker.setVisible(true);
+    });
+  }
+
+  //we initliaze the map and the markers below
+	init();
+
+  ['Schools', 'Companies', 'Parking'].forEach(function(query){
+    setMarkers(query);
+  });
+
+}());
